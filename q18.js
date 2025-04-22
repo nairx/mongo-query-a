b1e9bb3acadb5af363f060d92a9dfb174c5242e8
nairx <nairxprv@gmail.com>
2025-04-22 13:35:10 +0530

    updated

---
+++ q18.js
@@ -0,0 +1,20 @@
+db.employees.find({
+    name:{$regex:"Cathy"}
+},{_id:0,name:1,salary:1})
+
+
+db.employees.find({
+    name:{$regex:"^C"}
+},{_id:0,name:1,salary:1})
+
+db.employees.find({
+    name:{$regex:"^c",$options:"i"}
+},{_id:0,name:1,salary:1})
+
+db.employees.find({
+    name:{$regex:"n$"}
+},{_id:0,name:1,salary:1})
+
+db.employees.find({
+    name:{$regex:"n"}
+},{_id:0,name:1,salary:1})
