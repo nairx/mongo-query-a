070ab02fcb91776f38ce3de729872926d4a667b4
nairx <nairxprv@gmail.com>
2025-04-22 14:57:58 +0530

    Updated

--- q19.js
+++ q19.js
@@ -20,34 +20,64 @@ db.posts.aggregate([
 //////////
 
 db.authors.aggregate([
-    {$lookup:{
-        from:"posts",
-        localField:"_id",
-        foreignField:"authorId",
-        as:"posts"
-    }},
-    {$unwind:"$posts"}
-
-])
-
+  {
+    $lookup: {
+      from: "posts",
+      localField: "_id",
+      foreignField: "authorId",
+      as: "posts",
+    },
+  },
+  { $unwind: "$posts" },
+]);
 
 db.authors.aggregate([
-    {$lookup:{
-        from:"posts",
-        localField:"_id",
-        foreignField:"authorId",
-        as:"posts"
-    }},
-])
+  {
+    $lookup: {
+      from: "posts",
+      localField: "_id",
+      foreignField: "authorId",
+      as: "posts",
+    },
+  },
+  { $match: { "posts.title": "title 5" } },
+]);
+
+db.authors.aggregate([
+  {
+    $lookup: {
+      from: "posts",
+      localField: "_id",
+      foreignField: "authorId",
+      as: "posts",
+    },
+  },
+  { $match: { "posts.title": "title 5" } },
+  { $unwind: "$posts" },
+]);
+
+db.authors.aggregate([
+  {
+    $lookup: {
+      from: "posts",
+      let: { authorId: "$_id" },
+      pipeline: [
+        { $match: { $expr: { $eq: ["$authorId", "$$authorId"] } } },
+        { $match: { $expr: { $eq: ["$title", "title 5"] } } },
+      ],
+      as:"posts"
+    },
+  },
+  {$unwind:"$posts"}
+]);
 
 db.posts.insertOne({
-    title:"title 75",
-    desc: "desc 75",
-    authorId:ObjectId('68075aff10180e78792f45df')
-})
+  title: "title 75",
+  desc: "desc 75",
+  authorId: ObjectId("68075aff10180e78792f45df"),
+});
 
 db.authors.insertOne({
-    name:"John",
-    email:"john@email.com"
-})
-
+  name: "John",
+  email: "john@email.com",
+});
