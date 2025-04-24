f815067350c37ec5781711e091feafa33748c70c
nairx <nairxprv@gmail.com>
2025-04-24 14:32:24 +0530

    Updated

--- q24.js
+++ q24.js
@@ -8,3 +8,9 @@ start mongod -replSet rs1 -logpath d:\mongo-replica\data1\1.log --dbpath d:\mong
 start mongod -replSet rs1 -logpath d:\mongo-replica\data2\2.log --dbpath d:\mongo-replica\data2\ --port 27019
 
 start mongod -replSet rs1 -logpath d:\mongo-replica\data3\3.log --dbpath d:\mongo-replica\data3\ --port 27020
+
+mongosh --port 27018
+config={_id:"rs1",members:[{_id:0,host:"127.0.0.1:27018"},{_id:1,host:"127.0.0.1:27019"},{_id:2,host:"127.0.0.1:27020"}]}
+rs.initiate(config)
+rs.config()
+rs.status()
