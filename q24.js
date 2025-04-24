ba6fef5d84a14668a4603b653de25bd9769b5c10
nairx <nairxprv@gmail.com>
2025-04-24 14:06:14 +0530

    Updated

---
+++ q24.js
@@ -0,0 +1,6 @@
+create a  folder mongoreplica
+
+inside create 3 new folders
+data1, data2, and data3
+
+start mongod -replSet rs1 -logpath d:\mongo-replica\data1\1.log --dbpath d:\mongo-replica\data1\ --port 27018
